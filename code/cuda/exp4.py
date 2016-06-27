392739cce152d0803a08fb3978a9067b1166ff95
Ubuntu <ubuntu@ip-10-255-63-164.us-west-2.compute.internal>
2016-06-27 19:05:00 +0000

    exp 4 change

--- code/cuda/exp4.py
+++ code/cuda/exp4.py
@@ -6,9 +6,9 @@ s = 4
 h_up = 2**9
 N_large = N * s - h_up * (s - 1)
 es = EpsScan(10,20)
-T = 10
+T = 500
 H = [148,431]
-mu = 7./(H[0]*H[1])**0.5
+mu = 8./(H[0]*H[1])**0.5
 
 def run(hypo = 'null'):
     if hypo == 'alt':
